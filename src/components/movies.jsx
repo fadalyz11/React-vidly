91f9e4597c069fc41a9e2f85e68b0c0f74f9517a
fadalyz11 <fadalyz11@gmail.com>
2019-02-18 15:25:13 +0200

    mosh work

---
+++ src/components/movies.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    let movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  handlePageChange = page => {
+    console.log("page change");
+  };
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) {
+      return <h1>nothing left To add</h1>;
+    }
+    return (
+      <div className="container">
+        <h1 className="mt-2 mb-3 text-center">My Training : {count} left</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={() => {
+                      this.handleDelete(movie);
+                    }}
+                    className="btn btn-lg btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
